54ed3230d91c73e39f2702a6bace50204163e171
asif-mohmd <asifasifpsps@gmail.com>
2024-01-28 17:48:32 +0530

    Tailwind, Redux-toolkit configured and basic structure created

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const AppLayout = () => {
+
+
+    return(
+       <div>
+        
+       </div>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<AppLayout/>)
