76a17f1d30c0e0620e58f3b02d8ac5bbfd91c4fa
asif-mohmd <asifasifpsps@gmail.com>
2024-01-28 16:47:07 +0530

    react configured

--- server/config/connecton.js
+++ server/config/connecton.js
@@ -1,16 +1,18 @@
-
-
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 
-const DB = process.env.MONGOLAB_URI;
+const database = process.env.MONGOLAB_URI;
 
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-     useUnifiedTopology: true,
-}).then(() =>{
-    console.log('Database connected..')
-})
+
+const connectDB = async () => {
+    mongoose.set("strictQuery", true);
+    const db = await mongoose.connect(database, { useUnifiedTopology: true, useNewUrlParser: true })
+        .then(() => console.log('Database connected'))
+        .catch(err => console.log(err + "DB not connected"));
+
+}
+
+module.exports = connectDB
--- server/index.js
+++ server/index.js
@@ -1,10 +1,55 @@
+const connectDB = require("./config/connecton")
+const PhoneBook = require('./models/PhoneBook')
+
 const express = require('express')
+
+
 const cors = require('cors')
 const app = express()
+
+connectDB()
 
 app.use(express.json())
 app.use(cors())
 const PORT = 8080
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}...`)
+})
+
+
+app.post('/add-phone', async(req,res) => {
+    console.log("workong")
+    const phoneNumber = new PhoneBook(req.body)
+    console.log(req.body)
+    try{
+        await phoneNumber.save()
+        res.status(201).json({
+            status: 'Success',
+            data : {
+                phoneNumber
+            }
+        })
+    }catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message : err
+        })
+    }
+})
+
+app.get('/get-phone', async (req,res) => {
+    const phoneNumbers = await PhoneBook.find({})
+    try{
+        res.status(200).json({
+            status : 'Success',
+            data : {
+                phoneNumbers
+            }
+        })
+    }catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message : err
+        })
+    }
 })
