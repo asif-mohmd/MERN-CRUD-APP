f586090af8095724a333dd2588bb873044e3a110
asif-mohmd <asifasifpsps@gmail.com>
2024-01-29 15:06:16 +0530

    Signup form completed and API called

--- client/src/components/user/Signup.jsx
+++ client/src/components/user/Signup.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 
 
 function Signup() {
 
-    
+  const [user,setUser] = useState({name:'',email:'',age:'',location:'',job:'',password:''})
+
+  const handleSubmit = async (e) =>{
+  e.preventDefault()
+
+  try{
+   await axios.post("/",user)
+  }catch(err){
+   console.log("axios something went wrong")
+  }
+  
+  }
+
+
   return (
     <div className="min-h-screen sm:flex sm:flex-row mx-0 justify-center bg-slate-100">
       <div className="flex justify-center self-center  z-10 shadow-2xl rounded-3xl  ">
@@ -14,7 +27,7 @@ function Signup() {
             <h3 className="font-semibold text-2xl text-gray-800">Sign up </h3>
             <p className="text-gray-500">Sign up to your account.</p>
           </div>
-          <form className="space-y-3">
+          <form className="space-y-3" onSubmit={handleSubmit}>
             <div className="space-y-1">
               <label className="text-sm font-medium text-gray-700 tracking-wide">
                 Name
@@ -23,6 +36,8 @@ function Signup() {
                 className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
                 type="text"
                 placeholder="Enter your name"
+                value={user.name}
+                onChange={(e)=>setUser({...user,name:e.target.value})}
               />
             </div>
 
@@ -34,6 +49,9 @@ function Signup() {
                 className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
                 type="email"
                 placeholder="mail@gmail.com"
+                value={user.email}
+                onChange={(e)=>setUser({...user,email:e.target.value})}
+                
               />
             </div>
 
@@ -45,6 +63,8 @@ function Signup() {
                 className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
                 type="text"
                 placeholder="Enter your age"
+                value={user.age}
+                onChange={(e)=>setUser({...user,age:e.target.value})}
               />
             </div>
 
@@ -56,6 +76,8 @@ function Signup() {
                 className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
                 type="text"
                 placeholder="Enter your Location"
+                value={user.location}
+                onChange={(e)=>setUser({...user,location:e.target.value})}
               />
             </div>
 
@@ -67,6 +89,8 @@ function Signup() {
                 className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
                 type="text"
                 placeholder="Enter your details"
+                value={user.job}
+                onChange={(e)=>setUser({...user,job:e.target.value})}
               />
             </div>
 
@@ -78,6 +102,8 @@ function Signup() {
                 className="w-full content-center text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
                 type="text"
                 placeholder="Enter your password"
+                value={user.password}
+                onChange={(e)=>setUser({...user,password:e.target.value})}
               />
             </div>
             <div className="flex items-center justify-between"></div>
