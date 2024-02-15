e514cec41324deeca437d5f276579caa86460946
asif-mohmd <asifasifpsps@gmail.com>
2024-02-15 11:17:24 +0530

    auth middleware change 1

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,12 +1,14 @@
 import jwt from "jsonwebtoken";
 
 export const authorization = (req, res, next) => {
+
     const token = req.cookies.access_token;
     console.log("authing :",token)
     if (!token) {
         console.log("tk not working")
       return res.sendStatus(403);
     }
+
     try {
       const data = jwt.verify(token, process.env.JWT_SECRET);
       req.userId = data.id;
@@ -15,4 +17,5 @@ export const authorization = (req, res, next) => {
     } catch {
       return res.sendStatus(403);
     }
+    
   };
